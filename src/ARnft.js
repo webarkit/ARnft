ec4fcc3c61ec546c04e54ddbd9f01dc2af84d7a9
Walter Perdan <github@kalwaltart.it>
2020-09-17 17:04:37 +0200

    upgrading version

--- src/ARnft.js
+++ src/ARnft.js
@@ -12,7 +12,7 @@ export default class ARnft {
     this.root.matrixAutoUpdate = false
     this.config = config
     this.listeners = {}
-    this.version = '0.7.2'
+    this.version = '0.7.3'
     console.log('ARnft ', this.version)
   }
 
