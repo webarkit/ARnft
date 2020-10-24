85bf1376a060ca24ffa6504ff385a0a7d3d9293e
Walter Perdan <github@kalwaltart.it>
2020-10-24 16:18:09 +0200

    packages update and new 0.7.8 version

--- src/ARnft.js
+++ src/ARnft.js
@@ -13,7 +13,7 @@ export default class ARnft {
     this.root.matrixAutoUpdate = false
     this.config = config
     this.listeners = {}
-    this.version = '0.7.7'
+    this.version = '0.7.8'
     console.log('ARnft ', this.version)
   }
 
