da9bc071572a5de750d0fe0c54a6f4ef342dd952
kalwalt <github@kalwaltart.it>
2021-06-03 23:27:48 +0200

    starting to add a minimal docs

--- src/ARnft.ts
+++ src/ARnft.ts
@@ -1,3 +1,38 @@
+/*
+ *  ARnft.ts
+ *  ARnft
+ *
+ *  This file is part of ARnft - WebARKit.
+ *
+ *  ARnft is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ARnft is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with ARnft.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  As a special exception, the copyright holders of this library give you
+ *  permission to link this library with independent modules to produce an
+ *  executable, regardless of the license terms of these independent modules, and to
+ *  copy and distribute the resulting executable under terms of your choice,
+ *  provided that you also meet, for each linked independent module, the terms and
+ *  conditions of the license of that module. An independent module is a module
+ *  which is neither derived from nor based on this library. If you modify this
+ *  library, you may extend this exception to your version of the library, but you
+ *  are not obligated to do so. If you do not wish to do so, delete this exception
+ *  statement from your version.
+ *
+ *  Copyright 2021 WebARKit.
+ *
+ *  Author(s): Walter Perdan @kalwalt https://github.com/kalwalt
+ *
+ */
 import Container from './utils/html/Container'
 import { ConfigData } from "./config/ConfigData";
 import Stats from 'stats.js'
@@ -19,6 +54,17 @@ export default class ARnft {
     public camData: string;
     private uuid: string;
     private version: string;
+
+    /**
+     * The **ARnft** constructor to create a new instance of the ARnft class. 
+     * Example code: 
+     * ```javascript
+     * const nft = new ARnft(640, 480, 'config.json');
+     * ```
+     * @param width (number) the width in pixels of the video camera.
+     * @param height (number) the height in pixels of the video camera.
+     * @param configUrl (string) the url of the config.json file
+     */
     constructor(width: number, height: number,configUrl: string){
         this.width = width
         this.height = height
@@ -28,7 +74,18 @@ export default class ARnft {
         this.version = version;
         console.log('ARnft ', this.version);
     }
-
+    
+    /**
+     * The init function let define the basic set-up for the NFT marker.
+     * Internally use the initialize function, that is responsible to load all the resources.
+     * @param width (number) the width in pixels of the video camera.
+     * @param height (number) the height in pixels of the video camera.
+     * @param markerUrl (string) the url of the marker (without the extension) 
+     * @param configUrl (string) the url of the config.json file
+     * @param stats (boolean) true if you want the stats.
+     * @param camera (boolean) true if you want the camera.
+     * @returns (object) the nft object.
+     */
     static async init (width: number, height: number, markerUrl: string, configUrl: string, stats: boolean, camera: boolean): Promise<object> {
         const _arnft = new ARnft(width, height, configUrl);
         return await _arnft.initialize(markerUrl, stats, camera).catch((error: any) => {
@@ -36,8 +93,15 @@ export default class ARnft {
             return Promise.reject(false);
         })
     }
-
-    public async initialize(markerUrl: string, stats: boolean, camera: boolean): Promise<object> {
+    
+    /**
+     * 
+     * @param markerUrl 
+     * @param stats 
+     * @param camera 
+     * @returns 
+     */
+    private async initialize(markerUrl: string, stats: boolean, camera: boolean): Promise<object> {
         var event = new Event("initARnft");
         document.dispatchEvent(event);
         console.log('ARnft init() %cstart...', 'color: yellow; background-color: blue; border-radius: 4px; padding: 2px');
