07a7c77ea9a8ffaed15b8d0af2a1a62d22df4bd0
kalwalt <github@kalwaltart.it>
2021-06-04 22:31:25 +0200

    added license and basic documentation

--- src/NFTWorker.ts
+++ src/NFTWorker.ts
@@ -1,3 +1,38 @@
+/*
+ *  NFTWorker.ts
+ *  ARnft
+ *
+ *  This file is part of ARnft - WebARKit.
+ *
+ *  ARnft is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ARnft is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with ARnft.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  As a special exception, the copyright holders of this library give you
+ *  permission to link this library with independent modules to produce an
+ *  executable, regardless of the license terms of these independent modules, and to
+ *  copy and distribute the resulting executable under terms of your choice,
+ *  provided that you also meet, for each linked independent module, the terms and
+ *  conditions of the license of that module. An independent module is a module
+ *  which is neither derived from nor based on this library. If you modify this
+ *  library, you may extend this exception to your version of the library, but you
+ *  are not obligated to do so. If you do not wish to do so, delete this exception
+ *  statement from your version.
+ *
+ *  Copyright 2021 WebARKit.
+ *
+ *  Author(s): Walter Perdan @kalwalt https://github.com/kalwalt
+ *
+ */
 import Worker from 'worker-loader?inline=no-fallback!./Worker';
 import { isMobile } from './utils/ARUtils'
 
@@ -13,14 +48,30 @@ export default class NFTWorker {
     private vh: number;
 
     private uuid: string;
-
+    
+    /**
+     * The NFTWorker constructor, to create a new instance of the NFTWorker class.
+     * @param markerURL the marker url of the NFT marker.
+     * @param w the width of the camera.
+     * @param h the height of the camera.
+     * @param uuid the uuid of the marker assigned by the ARnft constructor.
+     */
     constructor(markerURL: string, w: number, h: number,  uuid: string) {
         this.markerURL = markerURL;
         this.vw = w;
         this.vh = h;
         this.uuid = uuid
     }
-
+    
+    /**
+     * Initialize the NFTWorker instance, you need to provide the camera_para.dat Url,
+     * the ImageData from the video stream and the renderUpdate and trackUpdate functions for the stats.
+     * @param cameraURL  
+     * @param imageData 
+     * @param renderUpdate 
+     * @param trackUpdate 
+     * @returns true if succesfull.
+     */
     public initialize(cameraURL: string, imageData: ImageData, renderUpdate: () => void, trackUpdate: () => void): Promise<boolean> {
         return new Promise<boolean>((resolve, reject) => {
             this.worker = new Worker();
@@ -34,7 +85,12 @@ export default class NFTWorker {
             });
         });
     }
-
+    
+    /**
+     * This is the function that will pass the video stream to the worker.
+     * @param imageData the image data from the video stream.
+     * @returns void
+     */
     public process(imageData: ImageData) {
 
         if (this._processing) {
@@ -44,7 +100,15 @@ export default class NFTWorker {
 
         this.worker.postMessage({ type: 'process', imagedata: imageData }, [imageData.data.buffer]);
     }
-
+    
+    /**
+     * Load the resources from the ARnft instance into the worker.
+     * @param cameraURL camera_para.dat url
+     * @param imageData image data from the video stream.
+     * @param renderUpdate renderUpdate function for the stats.
+     * @param trackUpdate trackUpdate for the stats.
+     * @returns true if succesfull.
+     */
     protected load(cameraURL: string, imageData: ImageData, renderUpdate: () => void, trackUpdate: () => void): Promise<boolean> {
 
         return new Promise<boolean>((resolve, reject) => {
@@ -136,7 +200,12 @@ export default class NFTWorker {
             this.process(imageData);
         });
     };
-
+    
+    /**
+     * dispatch an event listener if the marker is lost or the matrix of the marker
+     * if found.
+     * @param msg message from the worker.
+     */
     public found (msg: any) {
         let world: any;
         if (!msg) {
@@ -158,7 +227,10 @@ export default class NFTWorker {
     public destroy(): void {
 
     }
-
+    
+    /**
+     * Stop the NFT tracking and the video streaming.
+     */
     static stopNFT () {
         console.log("Stop NFT");
         var event = new Event("terminateWorker");
