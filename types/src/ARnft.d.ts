a2dd2ca2e28086699844f64cdfeb635912acb746
kalwalt <info@kalwaltart.it>
2021-10-09 22:32:28 +0200

    new target for Events

--- types/src/ARnft.d.ts
+++ types/src/ARnft.d.ts
@@ -15,21 +15,14 @@ export default class ARnft {
     camData: string;
     private controllers;
     private static entities;
+    private target;
     private uuid;
     private version;
     constructor(width: number, height: number, configUrl: string);
     static init(width: number, height: number, markerUrls: Array<string>, names: Array<string>, configUrl: string, stats: boolean): Promise<object>;
     static initWithEntities(width: number, height: number, entities: Entity[], configUrl: string, stats: boolean): Promise<object>;
     private _initialize;
-    private converter;
     static getEntities(): Entity[];
-    dispatchEvent(event: {
-        name: string;
-        target: any;
-        data?: object;
-    }): void;
-    addEventListener(name: string, callback: object): void;
-    removeEventListener(name: string, callback: object): void;
     dispose(): void;
     disposeNFT(): void;
     disposeVideoStream(): void;
