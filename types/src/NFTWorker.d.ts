bb0b5535ccc316e417d6a906edd8242b9d2a0ac0
Albatroz Jeremias <ajeremias@disroot.org>
2021-10-12 12:14:15 +0100

    set all nfts loaded event

--- types/src/NFTWorker.d.ts
+++ types/src/NFTWorker.d.ts
@@ -7,11 +7,13 @@ export default class NFTWorker {
     private target;
     private uuid;
     private name;
+    protected ready: boolean;
     constructor(markerURL: string, w: number, h: number, uuid: string, name: string);
     initialize(cameraURL: string, imageData: ImageData, renderUpdate: () => void, trackUpdate: () => void): Promise<boolean>;
     process(imageData: ImageData): void;
     protected load(cameraURL: string, imageData: ImageData, renderUpdate: () => void, trackUpdate: () => void): Promise<boolean>;
     found(msg: any): void;
+    isReady(): boolean;
     destroy(): void;
     static stopNFT(): void;
 }
