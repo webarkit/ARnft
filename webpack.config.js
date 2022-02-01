ad4b1b446f737d5f79090f898f9c3c83726d3008
kalwalt <github@kalwaltart.it>
2022-02-01 16:33:33 +0100

    added TerserPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: './src/index.ts',
@@ -49,5 +50,13 @@ module.exports = {
       jsartoolkitnft: '@webarkit/jsartoolkit-nft'
     },
     extensions: ['.tsx', '.ts', '.js'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+      }),
+    ],
   },
 }
