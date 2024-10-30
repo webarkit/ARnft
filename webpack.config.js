ff6aa3c2c9dbd3e3a4075c2fcf090685f2c1b6f7
Walter Perdan <github@kalwaltart.it>
2024-10-30 22:34:22 +0100

    module version of ARnft - flamingo and basic example with new three.module.js - all required libs in the repository

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,13 @@
 const path = require('path')
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'ARnft.js',
-    library: 'ARnft',
-    libraryTarget: 'umd',
-    // @see: https://github.com/webpack/webpack/issues/3929
-    libraryExport: 'default',
-    // @see: https://github.com/webpack/webpack/issues/6522
-    globalObject: "typeof self !== 'undefined' ? self : this",
-    publicPath: "auto"
-  },
-  externals: {
-    stats: {
-      commonjs: 'stats.js',
-      commonjs2: 'stats.js',
-      amd: 'stats.js',
-      root: 'Stats' // indicates global variable
-    }
-  },
-  module: {
+module.exports = (env, argv) => {
+  let devtool = false;
+  if (argv.mode === 'development') {
+    devtool = 'inline-source-map';
+  }
+  console.log(`${argv.mode} build`);
+  const module= {
     rules: [
       {
         test: /\.worker\.(c|m)?ts$/i,
@@ -32,26 +16,76 @@ module.exports = {
           options: { inline: 'no-fallback' }
         }
       },
-    {
-      test: /\.tsx?$/,
-      exclude: /node_modules/,
-      loader: 'ts-loader'
-    }
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }
     ]
-  },
-  resolve: {
+  };
+  const resolve = {
     alias: {
       jsartoolkitnft: '@webarkit/jsartoolkit-nft'
     },
     extensions: ['.tsx', '.ts', '.js'],
+  };
+  const optimization = {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+                             parallel: true,
+                             extractComments: false
+                           }),
+        ],
+      };
+  return [{
+    name: "ARnft",
+    entry: './src/index.ts',
+    devtool,
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'ARnft.js',
+      library: 'ARnft',
+      libraryTarget: 'umd',
+      // @see: https://github.com/webpack/webpack/issues/3929
+      libraryExport: 'default',
+      // @see: https://github.com/webpack/webpack/issues/6522
+      globalObject: "typeof self !== 'undefined' ? self : this",
+      publicPath: "auto"
+    },
+    externals: {
+      stats: {
+        commonjs: 'stats.js',
+        commonjs2: 'stats.js',
+        amd: 'stats.js',
+        root: 'Stats' // indicates global variable
+      }
+    },
+    module,
+    resolve,
+    optimization,
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-        extractComments: false
-      }),
-    ],
-  },
+    {
+      name: "ARnft.module",
+      entry: './src/index.ts',
+      devtool,
+      experiments: {
+        outputModule: true,
+      },
+      output: {
+        library: {
+          type: 'module',
+        },
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'ARnft.module.js',
+      },
+      externalsType: 'module',
+      externals: {
+        three: 'three',
+      },
+      module,
+      resolve,
+      optimization,
+    },
+  ]
 }
